f25c5db8ab465a0f00be01da49b86b4485f76b65
Leonardo Machado <leonardo.machado@kaefer.com>
2024-07-25 13:23:06 -0300

    Feat: Protocolo HTTP

--- src/Program.cs
+++ src/Program.cs
@@ -5,15 +5,13 @@ public class Program
     private string _connectionStringOrquest;
     private string _connectionStringDW;
     private int _tamPacote;
-    private int _sistema;
     public Program()
     {
         var configVariables = new Dictionary<string, string>
         {
             { "PACKET_SIZE", Environment.GetEnvironmentVariable("PACKET_SIZE")?? "n/a" },
             { "DW_CONNECTIONSTRING", Environment.GetEnvironmentVariable("DW_CONNECTIONSTRING")?? "n/a" },
-            { "ORQUEST_CONNECTIONSTRING", Environment.GetEnvironmentVariable("ORQUEST_CONNECTIONSTRING")?? "n/a" },
-            { "QUERY_SYSTEMID", Environment.GetEnvironmentVariable("QUERY_SYSTEMID")?? "n/a" }
+            { "ORQUEST_CONNECTIONSTRING", Environment.GetEnvironmentVariable("ORQUEST_CONNECTIONSTRING")?? "n/a" }
         };
 
         var anyConfigNotSet = configVariables.Any(variable => variable.Value == "n/a");
@@ -26,21 +24,15 @@ public class Program
         _tamPacote = int.Parse(configVariables["PACKET_SIZE"]);
         _connectionStringDW = configVariables["DW_CONNECTIONSTRING"];
         _connectionStringOrquest = configVariables["ORQUEST_CONNECTIONSTRING"];
-        _sistema = int.Parse(configVariables["QUERY_SYSTEMID"]);
     }
     public void Run()
     {
-        Agenda agenda = new();
-        agenda.Agendador(
-            _connectionStringOrquest, // Orquestrador
-            _connectionStringDW, // Datawarehouse
-            _tamPacote, // Tamanho do pacote
-            _sistema // Sistema de extração
-        );
+        HttpHost host = new(_connectionStringOrquest, _connectionStringDW, _tamPacote);
+        host.Run();
     }
     public static void Main()
     {  
-        Program runnable = new();
-        runnable.Run();
+        var program = new Program();
+        program.Run();
     }
 }
--- src/metadado/MetadadoAgenda.cs
+++
@@ -1,7 +0,0 @@
-namespace IntegraCs;
-public class AgendaInfo
-{
-    public required Action<int> Action {get; set;}
-    public required TimeSpan Tempo {get; set;}
-    public required bool IsRunning {get; set;}
-}
